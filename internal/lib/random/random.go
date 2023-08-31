9b347bc6faf716725b9249b20d54556feeaeab28
Alexandr Solomevich <alexandr.solomevich@gmail.com>
2023-08-31 15:46:04 +0300

    Add random string generator

---
+++ internal/lib/random/random.go
@@ -0,0 +1,17 @@
+package random
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func NewRandomString(n int) string {
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rnd.Intn(len(letters))]
+	}
+	return string(b)
+}
